d0dc2dfa46ba94f4058abef26b865a87e65ac339
TommikMontanov <151466306+TommikMontanov@users.noreply.github.com>
2025-05-07 15:48:44 +0500

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Получение переменных окружения
-API_TOKEN = os.getenv("BOT_TOKEN")
+API_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 ASF_API_URL = os.getenv("ASF_API_URL", "http://localhost:1242/ASF")
 ASF_API_KEY = os.getenv("ASF_API_KEY")
 
